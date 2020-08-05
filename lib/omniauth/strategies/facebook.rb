ab0a19717aad471c9b6d85959835796774f85929
Aurelien Violette <aurelien.v@webgroup-limited.com>
2020-08-05 13:04:39 +0200

    Handle redirect_uri param from client.

--- lib/omniauth/strategies/facebook.rb
+++ lib/omniauth/strategies/facebook.rb
@@ -80,7 +80,8 @@ module OmniAuth
           ''
         else
           # Fixes regression in omniauth-oauth2 v1.4.0 by https://github.com/intridea/omniauth-oauth2/commit/85fdbe117c2a4400d001a6368cc359d88f40abc7
-          options[:callback_url] || (full_host + script_name + callback_path)
+          # Handle configurable redirect_uri (see https://github.com/simi/omniauth-facebook/pull/310/commits/cfa14ea0345570e7aa0789b8a073742d1d79918c)
+          request.params['redirect_uri'] || options[:callback_url] || (full_host + script_name + callback_path)
         end
       end
 
